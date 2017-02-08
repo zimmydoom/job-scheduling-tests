8dfc7fef82782c9c5e7eece858179fb096bb715c
David Mendez Lopez <zimmydoom@iMac.local>
2017-02-08 15:07:01 -0600

    Added platform generator skeleton

---
+++ platform-generator/platform_gen.py
@@ -0,0 +1,29 @@
+'''
+Created February 1st 2017
+
+@author: zimmydoom
+'''
+
+from xml.etree.ElementTree import Element, SubElement, tostring
+import random
+
+__xml_text__ = "<?xml version='1.0'?>\n<!DOCTYPE platform SYSTEM \"http://simgrid.gforge.inria.fr/simgrid.dtd\">\n<platform version=\"3\">\n"
+__number_nodes__= 0
+__minimun_flops__ = 1000000
+
+
+def build_AS(number_nodes,type):
+    
+    return
+
+
+def main():
+    s = 'Here we go'
+    print (s)
+    
+    
+    
+
+
+if __name__ == '__main__':
+   main()
